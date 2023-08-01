5d1b6073d3e263a1ff72153fadddb82eac529b99
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-08-01 20:42:59 +0300

    1

--- src/main/java/drealm/block/DRBlockBrick.java
+++
@@ -1,9 +0,0 @@
-package drealm.block;
-
-import lotr.common.block.LOTRBlockBrickBase;
-
-public class DRBlockBrick extends LOTRBlockBrickBase {
-	public DRBlockBrick() {
-		setBrickNames("windDwarven", "windDwarvenCarved");
-	}
-}
