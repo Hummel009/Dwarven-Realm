a64773a231fec5a679e5bdee66f52b5c521189df
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-02-14 22:29:59 +0300

    big fix

--- src/main/java/com/github/hummel/drealm/handler/ForgeEventHandler.java
+++ src/main/java/com/github/hummel/drealm/handler/ForgeEventHandler.java
@@ -3,11 +3,14 @@ package com.github.hummel.drealm.handler;
 import com.github.hummel.drealm.listener.ReloadListener;
 import com.github.hummel.drealm.render.RenderLargeItem;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraftforge.client.event.TextureStitchEvent;
 
 public class ForgeEventHandler {
 	@SubscribeEvent
+	@SideOnly(Side.CLIENT)
 	public void preTextureStitch(TextureStitchEvent.Pre event) {
 		TextureMap map = event.map;
 		if (map.getTextureType() == 1) {
